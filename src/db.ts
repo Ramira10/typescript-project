e47f9fc4cfec3f03baa7ffc9e76e6f189542e3be
Ramira10 <matiasramira11@hotmail.com>
2022-07-07 16:17:03 -0300

    fix

--- src/db.ts
+++ src/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 (async () => {
     try {
-        const db = await mongoose.connect(`${process.env.MONGODB_URL}`);
+        const db = await mongoose.connect(`${config.MONGODB_URL}`);
         console.log('Database is connected to: ', db.connection.name)
     } catch (error) {
         console.log(error)
